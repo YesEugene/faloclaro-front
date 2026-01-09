b0a44a07210de2d3321ef9ca38c41a85c4ae39b6
Leo <yes.stroynov@gmail.com>
2026-01-09 10:03:45 +0000

    🎨 Align progress bar and navigation panel with task card - add left padding, remove gap

--- components/subscription/tasks/RulesTask.tsx
+++ components/subscription/tasks/RulesTask.tsx
@@ -608,8 +608,8 @@ export default function RulesTask({ task, language, onComplete, isCompleted, onN
       </div>
 
       {/* Progress Bar - Above navigation panel */}
-      <div className="fixed bottom-[70px] left-0 right-0 bg-white z-30" style={{ marginBottom: '0px', borderRadius: '0px', borderTopLeftRadius: '0px', borderTopRightRadius: '0px', borderBottomRightRadius: '0px', borderBottomLeftRadius: '0px', opacity: 1, color: 'rgba(23, 23, 23, 1)', verticalAlign: 'bottom', height: '33px' }}>
-        <div className="max-w-md mx-auto" style={{ paddingTop: '2px', paddingBottom: '6px', paddingLeft: '0px', paddingRight: '16px', background: 'unset', backgroundColor: 'unset' }}>
+      <div className="fixed bottom-[69px] left-0 right-0 bg-white z-30" style={{ marginBottom: '0px', borderRadius: '0px', borderTopLeftRadius: '0px', borderTopRightRadius: '0px', borderBottomRightRadius: '0px', borderBottomLeftRadius: '0px', opacity: 1, color: 'rgba(23, 23, 23, 1)', verticalAlign: 'bottom', height: '33px' }}>
+        <div className="max-w-md mx-auto" style={{ paddingTop: '2px', paddingBottom: '0px', paddingLeft: '16px', paddingRight: '16px', background: 'unset', backgroundColor: 'unset' }}>
           <div className="space-y-2">
             {/* Progress Text */}
             <div className="flex justify-between items-center" style={{ fontSize: '10px', color: 'rgba(23, 23, 23, 1)' }}>
@@ -636,7 +636,7 @@ export default function RulesTask({ task, language, onComplete, isCompleted, onN
 
       {/* Navigation Panel - Fixed at bottom (Cross-task navigation) */}
       <div className="fixed bottom-0 left-0 right-0 bg-white border-t border-gray-200 z-30" style={{ borderRadius: '0px', borderTopLeftRadius: '0px', borderTopRightRadius: '0px', borderBottomRightRadius: '0px', borderBottomLeftRadius: '0px', height: '69px', verticalAlign: 'bottom', marginBottom: '0px', opacity: 1, color: 'rgba(0, 0, 0, 1)' }}>
-        <div className="max-w-md mx-auto pt-3 pb-3" style={{ paddingBottom: 'env(safe-area-inset-bottom, 12px)', height: '69px', color: 'rgba(0, 0, 0, 1)', paddingLeft: '0px', paddingRight: '16px' }}>
+        <div className="max-w-md mx-auto pt-3 pb-3" style={{ paddingBottom: 'env(safe-area-inset-bottom, 12px)', height: '69px', color: 'rgba(0, 0, 0, 1)', paddingLeft: '16px', paddingRight: '16px' }}>
           <div className="flex items-center justify-between gap-4">
             {/* Previous Button - Left */}
             {canGoPrevious && onPreviousTask ? (
